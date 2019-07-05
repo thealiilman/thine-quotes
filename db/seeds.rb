04c4635cdcefe16e29b90ddf6e7be98f2e17cc38
thealiilman <ali@ali-ilman.com>
2019-07-06 07:24:06 +0800

    Added DB seeding.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating 10 quotes...'
+FactoryBot.create_list(:quote, 10)
+puts "You now have #{Quote.count} quotes!"
